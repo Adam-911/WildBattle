f7ca5b0d5a02f92a8bc24586f44276833f70f3d3
Adam-911 <40604826+Adam-911@users.noreply.github.com>
2019-11-02 22:15:46 +0600

    Добавлены исходники проекта в репозиторий

---
+++ Assets/Scripts/CharacteController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacteScript : MonoBehaviour
+{
+    public float movespeed = 10f;
+    private bool isFacingRight = true;
+    private Rigidbody2D rigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void run()
+    {
+        float move = Input.GetAxis("Horizontal");
+        rigidbody.velocity = new Vector2(move * movespeed, rigidbody.velocity.y);
+        if (move > 0 && !isFacingRight)
+            flip();
+        else if (move < 0 && isFacingRight)
+            flip();
+    }
+
+    void flip()
+    {
+        isFacingRight = !isFacingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+}
